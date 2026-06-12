9a570b7afd5f7721e9c9b77a4fc30f635d01ddae
Matías Zabaljáuregui <mzabaljauregui@nowvertical-es.com>
2026-06-11 22:35:26 -0300

    refactor: rename to /blog, remove NowVertical, reducir pesos tipográficos

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://matiasz-nowvertical.github.io',
+  base: '/blog',
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
